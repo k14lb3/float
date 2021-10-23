10c10606154d2e3b5c6ba8818fe55c0e009cdbae
k14lb3 <k14lb3@gmail.com>
2021-10-23 20:43:34 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import os
+import cv2 as cv
+import mediapipe as mp
+
+
+CAPTURE_WIDTH, CAPTURE_HEIGHT = 1280, 720
+PATH_IMAGES = "images"
+DIRECTORY = os.listdir(PATH_IMAGES)
+
+
+def main():
+
+    mp_drawing = mp.solutions.drawing_utils
+    mp_hands = mp.solutions.hands
+
+    cap = cv.VideoCapture(0)
+    cap.set(3, CAPTURE_WIDTH)
+    cap.set(4, CAPTURE_HEIGHT)
+
+    with mp_hands.Hands(
+        min_detection_confidence=0.5, min_tracking_confidence=0.5
+    ) as hands:
+        while cap.isOpened():
+            success, frame = cap.read()
+
+            frame.flags.writeable = False
+            frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+            results = hands.process(frame)
+
+            frame.flags.writeable = True
+            frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
+
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(
+                        frame,
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS,
+                    )
+
+            cv.imshow("Float", cv.flip(frame, 1))
+
+            if cv.waitKey(1) & 0xFF == ord("q"):
+                break
+
+        cap.release()
+
+
+if __name__ == "__main__":
+    main()
